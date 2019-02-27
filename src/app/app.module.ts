9b5b32958021c2405ecf523f49f39e0689d76190
wyyx <gnehcyx@163.com>
2019-02-27 15:40:37 +0800

    scroll to page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppComponent } from './app.component'
 import { NavComponent } from './nav/nav.component'
 import { SharedModule } from './shared/shared.module'
 import { PageComponent } from './nav/page/page.component'
+import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core'
+import { NgxPageScrollModule } from 'ngx-page-scroll'
 
 @NgModule({
   declarations: [AppComponent, NavComponent, PageComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, SharedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    NgxPageScrollCoreModule.forRoot({ _logLevel: 3 }),
+    NgxPageScrollModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,14 +1,28 @@
-import { Component, ElementRef } from '@angular/core'
+import {
+  Component,
+  ElementRef,
+  Inject,
+  ViewChild,
+  AfterViewInit,
+  Renderer2,
+  OnInit
+} from '@angular/core'
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
+import { PageScrollInstance, PageScrollService } from 'ngx-page-scroll-core'
+import { DOCUMENT } from '@angular/common'
+import { MatSidenavContent } from '@angular/material'
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements AfterViewInit, OnInit {
+  @ViewChild('scrollContainer', { read: ElementRef }) scrollContainer: ElementRef
+  @ViewChild('toolbar', { read: ElementRef }) toolbar: ElementRef
+
   ngTaskIngredients = [
     { label: 'JavaScript', imageUrl: '/assets/javascript_logo.png' },
     { label: 'TypeScript', imageUrl: '/assets/typescript_logo.png' },
@@ -30,9 +44,48 @@ export class NavComponent {
     .observe(Breakpoints.Handset)
     .pipe(map(result => result.matches))
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    @Inject(DOCUMENT) private document: any,
+    private pageScrollService: PageScrollService,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    console.log(this.scrollContainer)
+    this.renderer.listen(this.scrollContainer.nativeElement, 'scroll', event => {
+      const top = this.scrollContainer.nativeElement.scrollTop
+      console.log('scrolling', top)
+      console.log(this.toolbar)
+
+      if (top > 200) {
+        this.renderer.addClass(this.toolbar.nativeElement, 'scrolled-nav')
+      } else {
+        this.renderer.removeClass(this.toolbar.nativeElement, 'scrolled-nav')
+      }
+    })
+  }
 
   scrollIntoView(el: Element) {
     el.scrollIntoView({ block: 'start', behavior: 'smooth' })
+  }
+
+  scrollToAppsPage(scrollContainer: MatSidenavContent) {
+    const pageScrollInstance: PageScrollInstance = this.pageScrollService.create({
+      document: this.document,
+      scrollTarget: '#app1',
+      scrollViews: [scrollContainer.getElementRef().nativeElement],
+      duration: 500,
+      easingLogic: function(t, b, c, d) {
+        t /= d / 2
+        if (t < 1) return (c / 2) * Math.pow(2, 10 * (t - 1)) + b
+        t--
+        return (c / 2) * (-Math.pow(2, -10 * t) + 2) + b
+      },
+      scrollOffset: 50
+    })
+    this.pageScrollService.start(pageScrollInstance)
   }
 }
