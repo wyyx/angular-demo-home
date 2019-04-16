531092ed59edbea09773a5972e64a667e562e04d
wyyx <gnehcyx@163.com>
2019-04-16 10:07:21 +0800

    refine page, nav component

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,30 +1,32 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { DOCUMENT } from '@angular/common'
 import {
+  AfterViewInit,
   Component,
   ElementRef,
   Inject,
-  ViewChild,
-  AfterViewInit,
+  OnInit,
   Renderer2,
-  OnInit
-} from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable, Subject } from 'rxjs';
-import { map, tap, takeUntil } from 'rxjs/operators';
-import { PageScrollInstance, PageScrollService } from 'ngx-page-scroll-core';
-import { DOCUMENT } from '@angular/common';
-import { MatSidenavContent } from '@angular/material';
+  ViewChild,
+  OnDestroy
+} from '@angular/core'
+import { MatSidenavContent } from '@angular/material'
+import { PageScrollInstance, PageScrollService } from 'ngx-page-scroll-core'
+import { Observable, Subject } from 'rxjs'
+import { map, takeUntil, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements AfterViewInit, OnInit {
-  kill$: Subject<any> = new Subject();
+export class NavComponent implements AfterViewInit, OnInit, OnDestroy {
+  kill$: Subject<any> = new Subject()
 
-  mode = 'side';
-  @ViewChild('scrollContainer', { read: ElementRef }) scrollContainer: ElementRef;
-  @ViewChild('toolbar', { read: ElementRef }) toolbar: ElementRef;
+  mode = 'side'
+  @ViewChild('scrollContainer', { read: ElementRef })
+  scrollContainer: ElementRef
+  @ViewChild('toolbar', { read: ElementRef }) toolbar: ElementRef
 
   ngTaskIngredients = [
     { label: 'JavaScript', imageUrl: '/assets/javascript_logo.png' },
@@ -34,7 +36,7 @@ export class NavComponent implements AfterViewInit, OnInit {
     { label: 'Ngrx', imageUrl: '/assets/ngrx_logo.png' },
     { label: 'Angular Router', imageUrl: '/assets/angular_router_logo.png' },
     { label: 'Sass', imageUrl: '/assets/sass_logo.png' }
-  ];
+  ]
 
   ngOshopIngredients = [
     { label: 'JavaScript', imageUrl: '/assets/javascript_logo.png' },
@@ -43,7 +45,7 @@ export class NavComponent implements AfterViewInit, OnInit {
     { label: 'Rxjs', imageUrl: '/assets/rxjs_logo.png' },
     { label: 'Angular Router', imageUrl: '/assets/angular_router_logo.png' },
     { label: 'Sass', imageUrl: '/assets/sass_logo.png' }
-  ];
+  ]
 
   vueMusicIngredients = [
     { label: 'JavaScript', imageUrl: '/assets/javascript_logo.png' },
@@ -51,10 +53,11 @@ export class NavComponent implements AfterViewInit, OnInit {
     { label: 'Vuex', imageUrl: '/assets/vuex_logo.png' },
     { label: 'Vue Router', imageUrl: '/assets/vue_router_logo.png' },
     { label: 'Sass', imageUrl: '/assets/sass_logo.png' }
-  ];
+  ]
+
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
-    .pipe(map(result => result.matches));
+    .pipe(map(result => result.matches))
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -70,53 +73,59 @@ export class NavComponent implements AfterViewInit, OnInit {
       .pipe(
         tap(state => {
           if (state.matches) {
-            this.mode = 'over';
+            this.mode = 'over'
           } else {
-            this.mode = 'side';
+            this.mode = 'side'
           }
         }),
         takeUntil(this.kill$)
       )
-      .subscribe();
+      .subscribe()
   }
 
   ngOnDestroy(): void {
-    this.kill$.next();
-    this.kill$.complete();
+    this.kill$.next()
+    this.kill$.complete()
   }
 
   ngAfterViewInit(): void {
-    this.renderer.listen(this.scrollContainer.nativeElement, 'scroll', event => {
-      const top = this.scrollContainer.nativeElement.scrollTop;
+    this.renderer.listen(
+      this.scrollContainer.nativeElement,
+      'scroll',
+      event => {
+        const top = this.scrollContainer.nativeElement.scrollTop
 
-      if (top > 200) {
-        this.renderer.addClass(this.toolbar.nativeElement, 'scrolled-nav');
-      } else {
-        this.renderer.removeClass(this.toolbar.nativeElement, 'scrolled-nav');
+        if (top > 200) {
+          this.renderer.addClass(this.toolbar.nativeElement, 'scrolled-nav')
+        } else {
+          this.renderer.removeClass(this.toolbar.nativeElement, 'scrolled-nav')
+        }
       }
-    });
+    )
   }
 
   scrollIntoView(el: Element) {
-    el.scrollIntoView({ block: 'start', behavior: 'smooth' });
+    el.scrollIntoView({ block: 'start', behavior: 'smooth' })
   }
 
   scrollToAppsPage(scrollContainer: MatSidenavContent) {
-    const pageScrollInstance: PageScrollInstance = this.pageScrollService.create({
-      document: this.document,
-      scrollTarget: '#app1',
-      scrollViews: [scrollContainer.getElementRef().nativeElement],
-      duration: 500,
-      easingLogic: function(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) {
-          return (c / 2) * Math.pow(2, 10 * (t - 1)) + b;
-        }
-        t--;
-        return (c / 2) * (-Math.pow(2, -10 * t) + 2) + b;
-      },
-      scrollOffset: 50
-    });
-    this.pageScrollService.start(pageScrollInstance);
+    const pageScrollInstance: PageScrollInstance = this.pageScrollService.create(
+      {
+        document: this.document,
+        scrollTarget: '#app1',
+        scrollViews: [scrollContainer.getElementRef().nativeElement],
+        duration: 500,
+        easingLogic: function(t, b, c, d) {
+          t /= d / 2
+          if (t < 1) {
+            return (c / 2) * Math.pow(2, 10 * (t - 1)) + b
+          }
+          t--
+          return (c / 2) * (-Math.pow(2, -10 * t) + 2) + b
+        },
+        scrollOffset: 50
+      }
+    )
+    this.pageScrollService.start(pageScrollInstance)
   }
 }
--- src/app/nav/page/page.component.ts
+++ src/app/nav/page/page.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, Input, OnDestroy } from '@angular/core'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { Subject } from 'rxjs'
+import { tap, takeUntil } from 'rxjs/operators'
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss']
 })
-export class PageComponent implements OnInit {
+export class PageComponent implements OnInit, OnDestroy {
+  constructor(private breakpoint: BreakpointObserver) {}
+  kill$: Subject<any> = new Subject()
+
   @Input() url: string
   @Input() imageUrl: string
   @Input() appName: string
   @Input() buttonColor: string
   @Input() ingredients: [{ label: string; imgUrl: string }]
-  constructor() {}
 
-  ngOnInit() {}
+  techListTitle = ''
+
+  ngOnInit() {
+    this.breakpoint
+      .observe([`${Breakpoints.Handset}`, `${Breakpoints.Small}`])
+      .pipe(
+        tap(state => {
+          if (state.matches) {
+            this.techListTitle = 'Related'
+          } else {
+            this.techListTitle = 'Related Technologies'
+          }
+        }),
+        takeUntil(this.kill$)
+      )
+      .subscribe()
+  }
+
+  ngOnDestroy(): void {
+    this.kill$.next()
+    this.kill$.complete()
+  }
 
   open() {
     window.open(this.url, '_blank')
